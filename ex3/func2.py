bf50036722cf5c40e2b6697f683668beb618e071
MobinaZmn <79357997+MobinaZmn@users.noreply.github.com>
2021-06-14 15:17:50 +0430

    Update func2.py

--- ex3/func2.py
+++ ex3/func2.py
@@ -1,9 +1,12 @@
 def func2(Val):
     Temp = ""
     value = 0
+    DotPos = 0
     for i in Val:
         if i != ".":
             Temp += i
+        else:
+            DotPos = Val.index(i)
 
     HelpValue = len(Temp) - 1
     Help = ['0','1','2','3','4','5','6','7','8','9']
@@ -11,4 +14,5 @@ def func2(Val):
     for x in Temp:
         value = value + (Help.index(x) * (10 ** (HelpValue)))
         HelpValue -= 1
+    value = value / (10 ** (DotPos)) 
     return value
